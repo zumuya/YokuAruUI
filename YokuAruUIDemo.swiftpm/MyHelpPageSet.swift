2da4a7be87f8f14553578f9c73d0203938ade7e3
zumuya <zumuya@gmail.com>
2023-10-24 16:43:48 +0900

    [Copyright] 2023

--- YokuAruUIDemo.swiftpm/MyHelpPageSet.swift
+++ YokuAruUIDemo.swiftpm/MyHelpPageSet.swift
@@ -58,7 +58,7 @@ import YokuAruHelpView
 	
 	var acknowledgmentItems: [HelpAcknowledgmentItem] { [
 		.init(name: "YokuAruUI", text: """
-Copyright © 2020-2022 zumuya
+Copyright © 2020-2023 zumuya
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 and associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
